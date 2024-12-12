af2ed5ac4a6602a5fb5d7c2dd9548101d36bb45f
Vivaldini3 <vivaashlamba@seattleacademy.org>
2024-12-12 18:35:21 +0000

    Project 2

---
+++ Projects/Project 2.py/Project2.py
@@ -0,0 +1,9 @@
+while True:
+    word = input ("What do you think grandma likes? ")  
+
+    if word in ["cat" , "dog", "rat"]:
+        print (f"Grandma likes {word} !")
+    else:
+        print (f"Grandma doesn't like {word}")
+
+    print ("")
